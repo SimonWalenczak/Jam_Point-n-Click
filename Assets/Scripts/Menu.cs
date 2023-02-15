9f87086caf15f85531526c9034ceda713846b4ff
Frostork <simon.walenczak@lacatholille.fr>
2023-02-15 09:53:13 +0100

    main menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,5 +6,20 @@ public class Menu : MonoBehaviour
     public void OnClickPlay()
     {
         SceneManager.LoadScene("GameCommon");
+    }
+
+    public void OnClickQuit()
+    {
+        //If we are running in a standalone build of the game
+#if UNITY_STANDALONE
+        //Quit the application
+        Application.Quit();
+#endif
+
+        //If we are running in the editor
+#if UNITY_EDITOR
+        //Stop playing the scene
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
